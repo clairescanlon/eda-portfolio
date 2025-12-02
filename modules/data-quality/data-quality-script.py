543af3b359e4f9bd160db32671807414dd01941e
Claire Scanlon <104991589+clairescanlon@users.noreply.github.com>
2025-12-02 13:46:12 -0500

    Update data-quality-script.py
    Removed List from the import statement.

--- modules/data-quality/data-quality-script.py
+++ modules/data-quality/data-quality-script.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from typing import Dict, List, Tuple, Any
+from typing import Dict, Tuple, Any
 import logging
 from enum import Enum
 import json
